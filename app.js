a3002a1f8ab5ec6fb33de13ea2d3f6e8da78e0ef
Imblessedpaul <139491066+Imblessedpaul@users.noreply.github.com>
2023-08-04 02:45:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_4susn0b", "template_h8v32hw", formData)
+    emailjs.send("service_0dn58q7", "template_mcsnp0a", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
